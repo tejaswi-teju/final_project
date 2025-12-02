89cffc8371fb5fdce960f5fe5276b02765577876
tejaswi <blahblah@gmail.com>
2025-12-02 09:58:22 -0500

    syntax error

--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -1,20 +1,5 @@
-variable "cidr" {
-    type = string
-    description = "This is my vpc"
-}
+variable "cidr" {}
 
-variable "public_subnet" {
-    type = map(object({
-        cidr = string
-        az = string
-    }))
-    description = "These are public subnets of vpc-1"
-}
+variable "public_subnet" {}
 
-variable "private_subnet" {
-    type = map(object({
-        cidr = string
-        az = string
-    }))
-    description = "These are private subnets for vpc-1"
-}
+variable "private_subnet" {}
